765144cdc60175ccf5acbb8fcda7ae62123335c7
Eidoron1 <eidoronp67@gmail.com>
2022-05-17 16:10:41 +0545

    Bringup for NAMELESS

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,27 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common Nameless stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-TARGET_BOOT_ANIMATION_RES := 108o
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
-ARCANA_DEVICE := alioth
-ARCANA_MAINTAINER := イタチ
-ARCANA_OFFICIAL := true
-WITH_GAPPS := true
+CUSTOM_BUILD_TYPE := Official
 
 TARGET_SUPPORTS_QUICK_TAP := true
 
-TARGET_SUPPORTS_NOW_PLAYING ?= true
+TARGET_SUPPORTS_NOW_PLAYING := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
